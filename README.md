1bf17a9f129ac0abb783c03b2da10a11bfe26316
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-02-16 23:59:36 -0300

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+<<<<<<< HEAD
+=======
+These are my Python projects. See everything you are looking and thanks for being here.
+>>>>>>> cd872b34cb79735ad72db6fe228e29d0082e8d4a
---
+++ treasure.py
@@ -0,0 +1,19 @@
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+left_right = str(input("You're across road.\nWhere do you want to go. Left or Right [L/R]")).upper()
+if left_right == "R":
+    print("Fall into a hole Game Over")
+else:
+    swim_wait = str(input('You come to a lake, There is an island in the middle of lake \ntype "wait" to wait a boat. type "swim" to swim across')).upper()
+    if swim_wait == "SWIM":
+        print("Attacked by trout. Gamer over")
+    else:
+        choice_door = str(input("You arrive at the island unharmed.\nThere is  3 doors. One red, yellow and one blue. Wich color do you choice")).upper()
+        if choice_door == "RED":
+            print("Burned by fire.Game Over")
+        elif choice_door == "BLUE":
+            print("Eaten by beasts. Game Over")
+        elif choice_door == "":
+            print("Game Over")
+        else:
+            print("Congratulations. You win!")
